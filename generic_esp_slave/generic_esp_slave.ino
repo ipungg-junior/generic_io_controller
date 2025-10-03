b5632a0c48adb045dbcefd5ce2e769fbdcd61720
ipungg-junior <ipungg.id@gmail.com>
2025-10-03 10:33:39 +0700

    templating ino file

--- generic_esp_slave/generic_esp_slave.ino
+++ generic_esp_slave/generic_esp_slave.ino
@@ -1,28 +1,14 @@
 #include <SPI.h>
 #include "EthernetManager.h"
 #include "WebService.h"
-#include "Parser.h"
 #include "PinController.h"
-#include "MySQLConnector.h"
-#include <ArduinoJson.h>
 #include "TransactionLog.h"
 
 // Network profile cofiguration
-IPAddress whitelist[] = {
-  IPAddress(10, 251, 12, 133),
-  IPAddress(10, 251, 12, 109),
-  IPAddress(10, 251, 2, 103)
-};
 byte mac[] = { 0xDE, 0xAA, 0xBE, 0xEF, 0x00, 0x02 };
-IPAddress staticIP(10, 251, 2, 126);
-IPAddress gateway(10, 251, 2, 1);
-IPAddress dns(10, 251, 2, 1);
-IPAddress subnet(255, 255, 255, 0);
-EthernetManager eth(mac, staticIP, dns, gateway, subnet);
+EthernetManager eth(mac);
 
 // Database configuration
-IPAddress mysql_address(10, 251, 2, 114);
-MySQLConnector mysql;
 TransactionLog logger;
 TransactionEntry entry;
 
@@ -32,257 +18,24 @@ WebService http(eth, 80);
 // GPIO configuration
 PinController pinController;
 
-// Sensor or relay or ext. module
-#include "Wiegand.h"
-WIEGAND wg;
-
-
-unsigned long prevMillisWiegand;
-
-
-void gpioHandling(EthernetClient& client, const String& path, const String& body) {
-  Parser parser(body);
-
-  if (!parser.isValid()) {
-    client.println("HTTP/1.1 400 Bad Request");
-    client.println("Content-Type: text/plain");
-    client.println("Connection: close");
-    client.println();
-    client.println("Invalid JSON");
-    return;
-  }
-
-  String cmd = parser.getCommand();
-
-  if (cmd == "set_pin") {
-    if (!parser.hasKey("pin_number")) {
-      client.println("HTTP/1.1 400 Bad Request");
-      client.println("Content-Type: application/json");
-      client.println("Connection: close");
-      client.println();
-      client.print("{\"status\":false,");
-      client.print("\"message\":\"Please set your pin!\"}");
-
-      return;
-    }
-    int pin = parser.getInt("pin_number");
-    if (!parser.hasKey("value")) {
-      client.println("HTTP/1.1 400 Bad Request");
-      client.println("Content-Type: application/json");
-      client.println("Connection: close");
-      client.println();
-      client.print("{\"status\":false,");
-      client.print("\"message\":\"Please set value!\"}");
-      return;
-    }
-    
-    int setVal = parser.getInt("value");
-    
-    // Check if auto_reverse parameter exists
-    if (parser.hasKey("auto_reverse")) {
-      int interVal = parser.getInt("auto_reverse");
-      pinController.setPin(pin, setVal, interVal);
-    } else {
-      // No auto reverse, just set the pin
-      pinController.setPin(pin, setVal, 0);
-    }
-
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: application/json");
-    client.println("Connection: close");
-    client.println();
-
-    client.print("{");
-    client.print("\"status\":true,");
-    client.print("\"pin_num\":");
-    client.print(pin);
-    client.print(", \"message\":\"Pin io setting up completed\"");
-    client.print("}");
-
-  }
-  else if (cmd == "off_all") {
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: application/json");
-    client.println("Connection: close");
-    client.println();
-
-    pinController.offAll();
-
-    client.print("{");
-    client.print("\"status\":true,");
-    client.print("\"message\":\"Set all GPIO to 0 (off)\"");
-    client.print("}");
-  }
-  else if (cmd == "on_all") {
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: application/json");
-    client.println("Connection: close");
-    client.println();
-
-    pinController.onAll();
-
-    client.print("{");
-    client.print("\"status\":true,");
-    client.print("\"message\":\"Set all GPIO to 1 (on)\"");
-    client.print("}");
-  }
-  else {
-    client.println("HTTP/1.1 400 Bad Request");
-    client.println("Content-Type: application/json");
-    client.println("Connection: close");
-    client.println();
-    client.print("{");
-    client.print("\"status\":false,");
-    client.print("\"message\":\"Unknown command on GPIO\"");
-    client.print("}");
-  }
-}
-
-void coreHandling(EthernetClient& client, const String& path, const String& body) {
-  Parser parser(body);
-
-  if (!parser.isValid()) {
-    client.println("HTTP/1.1 400 Bad Request");
-    client.println("Content-Type: text/plain");
-    client.println("Connection: close");
-    client.println();
-    client.println("Invalid JSON");
-    return;
-  }
-
-  String cmd = parser.getCommand();
-  if (cmd == "restart") {
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/plain");
-    client.println("Connection: close");
-    client.println();
-    client.print("{");
-    client.print("\"status\":true,");
-    client.print("\"message\":\"Trying to restart, see you :)\"");
-    client.print("}");
-    ESP.restart();
-  }
-  else if (cmd == "register_card") {
-
-    // valid requets will change wgMode to register mode
-    unsigned long startChangeMode = millis();
-    while (true){
-
-      unsigned long currentMillis = millis();
-
-      if (currentMillis - startChangeMode >= 3000) {
-        client.println("HTTP/1.1 400 Bad Request");
-        client.println("Content-Type: application/json");
-        client.println("Connection: close");
-        client.println();
-        client.print("{\"status\":true,");
-        client.print("\"message\":\"Wiegand timeout\"}");
-        break;
-      } 
-      else {
-
-        if (wg.available()){
-          String wgData = String(wg.getCode());
-
-          // Skip noise
-          if (wgData.length() < 6){
-            return;
-          }
-          Serial.print("Found for register : ");
-          Serial.println(wgData);
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: application/json");
-          client.println("Connection: close");
-          client.println();
-          client.print("{");
-          client.print("\"status\":true,");
-          client.print("\"uid\":");
-          client.print(wgData);
-          client.print(", \"message\":\"Found card uid\"");
-          client.print("}");
-          break;
-
-        }
-      }
-
-    }
-
-  }
-  else {
-    client.println("HTTP/1.1 400 Bad Request");
-    client.println("Content-Type: application/json");
-    client.println("Connection: close");
-    client.println();
-    client.print("{");
-    client.print("\"status\":false,");
-    client.print("\"message\":\"Unknown command on core\"");
-    client.print("}");
-  }
-}
-
-bool validateCardId(String cardNumber) {
-
-  // Example using the new selectQueryf method with QueryResult and variable parameters
-  QueryResult result;
-  if (mysql.selectQueryf(result, "SELECT employee_card.id, employee.name FROM employee_card JOIN employee ON employee.id = employee_card.employee_id WHERE employee_card.card_number = '%s'", cardNumber)) {
-    String id;
-    String name;
-    // Process each row
-    for (int i = 0; i < result.size(); i++) {
-      RowData& row = result[i];
-      if (row.values.size() >= 2) {
-        id = row.values[0];
-        name = row.values[1];
-        Serial.print("ID: ");
-        Serial.print(id);
-        Serial.print(", Name: ");
-        Serial.println(name);
-      }      
-    }
-    entry.id = static_cast<uint16_t>((id.toInt()));
-    snprintf(entry.uid, sizeof(entry.uid), cardNumber.c_str());
-    logger.add(entry);
-
-    mysql.closeCursor();
-    
-    return result.size() > 0;
-  }else{
-    mysql.closeCursor();
-    return false;
-  }
-  
-}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(32, OUTPUT);
 
   // Network ethernet setup
   eth.begin(5);
-  eth.setWhitelist(whitelist, sizeof(whitelist) / sizeof(whitelist[0]));
   
   // Webservice Setup
   http.begin();
-  http.on("/core", coreHandling);
-  http.on("/gpio", gpioHandling);
 
   // Setup button pins
   pinController.setPinAsInput(13);
   pinController.setPinAsInput(14);
-  
-  // Setup MySQL connection (example configuration)
-  if (mysql.connect(mysql_address, 3306, "uprod", "P@ssw0rd*1", "doorlock")) {
-    Serial.println("Connected to MySQL database");
-  } else {
-    Serial.println("Failed to connect to MySQL database");
-  }
 
-  // Wiegand scanner RFID setup
-  wg.begin(16, 17);
   
+  // Show log from flash-mem
   logger.begin();
-  // Tampilkan semua log
   for (int i = 0; i < logger.size(); i++) {
       entry = logger.get(i);
       Serial.print("Log #"); Serial.print(i);
@@ -314,35 +67,6 @@ void loop() {
   if (pinController.getState(14) == 1) {
     // Button on pin 15 is pressed, do something
     pinController.setPin(32, 1, 1500);
-  }
-  
-
-  // Wiegand routine
-  if (wg.available()){
-
-    // check if interval has passed
-    if (currentMillis - prevMillisWiegand >= 2000) {
-      bool is_opened = false;
-      prevMillisWiegand = currentMillis;  // save the last time
-      String wgData = String(wg.getCode());
-
-      // Skip noise
-      if (wgData.length() < 6){
-        return;
-      }
-
-      if (validateCardId(wgData)){
-        if (!is_opened){
-          is_opened = true;
-          pinController.setPin(32, 1, 1500);
-          
-        }
-      }
-
-      Serial.print("Wiegand scan result : ");
-      Serial.println(wgData);
-
-    }
   }
   
                    
